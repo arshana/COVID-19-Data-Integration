982325083642a2f62f0c701df3b03907f0576022
PatrickMurphy99 <71674198+PatrickMurphy99@users.noreply.github.com>
2022-03-08 21:15:09 -0800

    big problems with sources opening
    source opening is creating weird bugs

--- frontendreact/src/components/ButtonOptions.tsx
+++ frontendreact/src/components/ButtonOptions.tsx
@@ -16,21 +16,35 @@ export default function ButtonOptions(props:specificLevel) {
     const strainsUrl:string = 'http://127.0.0.1:8000/info-from-area/?area=' + props.geoLevel + '&info-type=strains&area-code=' + props.geoCode;
     const populationUrl:string = 'http://127.0.0.1:8000/info-from-area/?area=' + props.geoLevel + '&info-type=population&area-code=' + props.geoCode;
     const ageUrl:string = 'http://127.0.0.1:8000/info-from-area/?area=' + props.geoLevel + '&info-type=age&area-code=' + props.geoCode;
+    const casesSourceUrl:string = 'http://127.0.0.1:8000/sources-from-info-and-area/?area=' + props.geoLevel + '&info-type=cases&area-code=' + props.geoCode;
+    const vaccinationsSourceUrl:string = 'http://127.0.0.1:8000/sources-from-info-and-area/?area=' + props.geoLevel + '&info-type=vaccinations&area-code=' + props.geoCode;
+    const strainsSourceUrl:string = 'http://127.0.0.1:8000/sources-from-info-and-area/?area=' + props.geoLevel + '&info-type=strains&area-code=' + props.geoCode;
+    const populationSourceUrl:string = 'http://127.0.0.1:8000/sources-from-info-and-area/?area=' + props.geoLevel + '&info-type=population&area-code=' + props.geoCode;
+    const ageSourceUrl:string = 'http://127.0.0.1:8000/sources-from-info-and-area/?area=' + props.geoLevel + '&info-type=age&area-code=' + props.geoCode;
     const [chosenUrl, setChosenUrl] = useState<string>("");
+    const [sourceUrl, setSourceUrl] = useState<string>("");
     const casesFunction = (event:React.MouseEvent<HTMLButtonElement>):void => {
         setChosenUrl(casesUrl);
+        setSourceUrl(casesSourceUrl);
     }
     const vaccinationsFunction = (event:React.MouseEvent<HTMLButtonElement>):void => {
         setChosenUrl(vaccinationsUrl);
+        setSourceUrl(vaccinationsSourceUrl);
     }
     const strainsFunction = (event:React.MouseEvent<HTMLButtonElement>):void => {
         setChosenUrl(strainsUrl);
+        setSourceUrl(strainsSourceUrl);
     }
     const populationFunction = (event:React.MouseEvent<HTMLButtonElement>):void => {
         setChosenUrl(populationUrl);
+        setSourceUrl(populationSourceUrl);
     }
     const agesFunction = (event:React.MouseEvent<HTMLButtonElement>):void => {
         setChosenUrl(ageUrl);
+        setSourceUrl(ageSourceUrl);
+    }
+    const onClickFunctionz = (event:React.MouseEvent<HTMLButtonElement>):void => {
+        console.log("please work");
     }
     return (
         <div>
@@ -42,7 +56,7 @@ export default function ButtonOptions(props:specificLevel) {
                 <Button onClickFunction={populationFunction} url={populationUrl} buttonName='population'/>
                 <Button onClickFunction={agesFunction} url={ageUrl} buttonName='age'/>
             </div>
-            <SourcesSelector url = {chosenUrl}/>
+            <SourcesSelector dataUrl = {chosenUrl} sourceUrl = {sourceUrl} onClickFunction={onClickFunctionz} />
         </div>
     );
 }
--- frontendreact/src/components/SourcesSelector.tsx
+++ frontendreact/src/components/SourcesSelector.tsx
@@ -1,26 +1,39 @@
 import React, { useEffect, useState } from 'react'
 
 interface SourceInterface {
-    url: string
+    dataUrl: string;
+    sourceUrl: string;
+    onClickFunction(event:React.MouseEvent<HTMLButtonElement>):void
 }
-
 export default function SourceSelector(props:SourceInterface) {
     const [sourceOptions, setSourceOptions] = useState<Object[]>([]);
+    // const checkedSources:Map<string, boolean> = new Map();
     useEffect(() => {
         const updateSourceOptions =  async () => {
-            const fetchedSourceOutput:Object[] = await (await fetch(props.url)).json();
+            const fetchedSourceOutput:Object[] = await (await fetch(props.sourceUrl)).json();
             setSourceOptions(fetchedSourceOutput)
         };
         updateSourceOptions();
-    }, [props.url]);
+    }, [props.sourceUrl, props.dataUrl]);
+    const onChangeFunction = (event:React.ChangeEvent<HTMLInputElement>):void => {
+        // checkedSources.set(event.target.value, !checkedSources.get(event.target.value));
+        // console.log(checkedSources);
+    }
     return (
-        <div>{ props.url !== "" ?
-            <><h2>Select any number of listed sources (you need to choose at least one)</h2><select multiple //onChange
-            > {sourceOptions.map((optionItem: any) => <option key={optionItem['source_id']}
-                value={optionItem['source_id'] + "#" + optionItem['source_information']}>
-                {optionItem["source_information"] + " (" + optionItem["source_id"] + ")"}</option>)}
-                <option value="ALL!">All available sources</option></select></>
-            : null}
-    </div>
+        <div>{ props.sourceUrl !== "" ?
+            <div>
+            <h2>Select any number of listed sources (you need to choose at least one)</h2>
+            <form>
+            {sourceOptions.map((optionItem: any) =>
+                    <div key={optionItem['source_id']}> 
+                        <label>{optionItem['source_information'] + " (" + optionItem['source_id'] + ")"}</label>
+                        <input type="checkbox" onChange = {onChangeFunction} value={optionItem['source_id']}/>
+                    </div>
+                    )}
+            <button onClick={props.onClickFunction}>Submit or die</button>
+            </form>
+            </div> : null}
+        </div>
+
     );
 }
